b682e96933f7c3489406ad8fafc22aec53758d59
joseluza <u202213404@upc.edu.pe>
2024-09-28 17:40:24 -0500

    refactor(web): MIgration Vue

--- assets/consolidacion.js
+++
@@ -1,8 +0,0 @@
-// Función para consolidar el reporte
-function consolidarReporte() {
-    // Aquí podrías agregar la lógica para generar el reporte consolidado
-    const reporte = "Este es el reporte consolidado de letras/facturas.";
-
-    // Mostrar el reporte consolidado en el div correspondiente
-    document.getElementById('reporte-consolidado').innerText = reporte;
-}
